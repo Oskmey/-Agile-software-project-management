40475c7f1cee75a6ca959220aee353530524da5c
Caramajau <caramia0505@gmail.com>
2024-04-08 16:50:06 +0200

    Created source data tests

---
+++ DAT257AgileProject/Assets/Tests/EditMode/Utility/SourceDataTests.cs
@@ -0,0 +1,91 @@
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[TestFixture]
+public class SourceDataTests
+{
+    private static readonly SourceData[] sources = Resources.LoadAll<SourceData>("ScriptableObjects");
+
+    public static IEnumerable<TestCaseData> TrashDataTestCases
+    {
+        get
+        {
+            foreach (var source in sources)
+            {
+                yield return new TestCaseData(source);
+            }
+        }
+    }
+
+    [Test]
+    public void Sources_AreNotNull()
+    {
+        Assert.IsNotNull(sources);
+    }
+
+    [Test]
+    public void Sources_AreNotEmpty()
+    {
+        Assert.IsNotEmpty(sources);
+    }
+
+    [Test, TestCaseSource(nameof(TrashDataTestCases))]
+    public void EachSource_HasSourceName(SourceData source)
+    {
+        Assert.IsNotNull(source.SourceName);
+    }
+
+    [Test, TestCaseSource(nameof(TrashDataTestCases))]
+    public void EachSource_HasDate(SourceData source)
+    {
+        Assert.IsNotNull(source.Date);
+    }
+
+    [Test, TestCaseSource(nameof(TrashDataTestCases))]
+    public void EachSource_HasTitle(SourceData source)
+    {
+        Assert.IsNotNull(source.Title);
+    }
+
+    [Test, TestCaseSource(nameof(TrashDataTestCases))]
+    public void EachSource_hasWebsite(SourceData source)
+    {
+        Assert.IsNotNull(source.Website);
+    }
+
+    [Test, TestCaseSource(nameof(TrashDataTestCases))]
+    public void EachSource_hasLink(SourceData source)
+    {
+        Assert.IsNotNull(source.Link);
+    }
+
+    [Test]
+    public void EachSource_hasUniqueLink()
+    {
+        var links = new HashSet<string>();
+        foreach (var source in sources)
+        {
+            Assert.IsTrue(links.Add(source.Link));
+        }
+    }
+
+    [Test, TestCaseSource(nameof(TrashDataTestCases))]
+    public void EachSource_hasValidLink(SourceData source)
+    {
+        bool uriCreationResult = Uri.TryCreate(source.Link, UriKind.Absolute, out Uri uriResult);
+        bool isHttpUrl = uriResult.Scheme == Uri.UriSchemeHttp;
+        bool isHttpsUrl = uriResult.Scheme == Uri.UriSchemeHttps;
+        bool isValid = uriCreationResult && (isHttpUrl || isHttpsUrl);
+
+        Assert.IsTrue(isValid, $"Invalid URL: {source.Link}");
+    }
+
+    [Test, TestCaseSource(nameof(TrashDataTestCases))]
+    public void EachSource_hasRetrievalDate(SourceData source)
+    {
+        Assert.IsNotNull(source.RetrievalDate);
+    }
+}
