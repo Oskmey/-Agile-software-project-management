84177bb020ff7206086ea902083313ebb2c656f7
Caramajau <caramia0505@gmail.com>
2024-04-08 14:33:52 +0200

    Added readable string method to trash category

--- DAT257AgileProject/Assets/Scripts/Trash/TrashCategory.cs
+++ DAT257AgileProject/Assets/Scripts/Trash/TrashCategory.cs
@@ -1,3 +1,5 @@
+using System;
+
 public enum TrashCategory
 {
     Organic,
@@ -6,4 +8,28 @@ public enum TrashCategory
     Glass,
     Metal,
     Electronic
+}
+
+public static class TrashCategoryExtensions
+{
+    public static string ToReadableString(this TrashCategory trashCategory)
+    {
+        switch (trashCategory)
+        {
+            case TrashCategory.Organic:
+                return "Organic";
+            case TrashCategory.Paper:
+                return "Paper";
+            case TrashCategory.Plastic:
+                return "Plastic";
+            case TrashCategory.Glass:
+                return "Glass";
+            case TrashCategory.Metal:
+                return "Metal";
+            case TrashCategory.Electronic:
+                return "Electronic";
+            default:
+                throw new ArgumentOutOfRangeException(nameof(trashCategory), trashCategory, $"Trash category not found {trashCategory}");
+        }
+    }
 }
