7d1fa96f2a6057c741ffa790b7654a7ac4ead063
HugoUnosson <hugo.unosson@gmail.com>
2024-04-16 16:41:58 +0200

    Cat fishes in correct direction

--- DAT257AgileProject/Assets/Scripts/Player/PlayerAnimations.cs
+++ DAT257AgileProject/Assets/Scripts/Player/PlayerAnimations.cs
@@ -7,6 +7,11 @@ public class PlayerAnimations : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animator;
 
+    private bool isFishing = false;
+    private bool isPlaying = false;
+    private Vector3 fishingSpotPos;
+    private Vector3 direction;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -16,16 +21,63 @@ public class PlayerAnimations : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        // TEMP Hardcoded for now
+        GameObject fishingSpot = GameObject.FindGameObjectWithTag("Fishing Spot");
+        if (fishingSpot != null)
+        {
+            fishingSpotPos = fishingSpot.transform.position;
+        }
+        else
+        {
+            Debug.Log("No fishing spot found");
+        }
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(rb.velocity.x);
+        UglyTempFunction();
+        CalculateDirectionToFishingSpot();
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
         animator.SetFloat("Horizontal", rb.velocity.x);
         animator.SetFloat("Vertical", rb.velocity.y);
         animator.SetFloat("Speed", rb.velocity.sqrMagnitude);
+        animator.SetBool("isFishing", isFishing);
+        animator.SetBool("isPlaying", isPlaying);
+        animator.SetFloat("Distance_x", direction.x);
+        animator.SetFloat("Distance_y", direction.y);
+    }
+
+    private void CalculateDirectionToFishingSpot()
+    {
+        direction = fishingSpotPos - transform.position;
+        direction = direction.normalized;
+    }
+
+    private void UglyTempFunction()
+    {
+        // TEMP!!!
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isFishing = true;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isPlaying = true;
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            isFishing = false;
+            isPlaying = false;
+        }
+
+        //isFishing = get...
+        //isPlaying = get...
     }
 }
 
