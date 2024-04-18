bc1b0a8ae7dc3626017fbf21c98b3f8fb6819b04
Figer-op <90894397+Figer-op@users.noreply.github.com>
2024-04-18 11:19:19 +0200

    added basic features for pause

---
+++ DAT257AgileProject/Assets/PauseMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+ 
+         Pause();
+
+        }  
+    }
+    
+
+   public void Pause()
+    {
+        if (GamePaused)
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GamePaused = false;
+        }
+        else
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GamePaused = true;
+        }
+
+    }
+}
