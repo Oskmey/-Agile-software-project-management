43572823d32f2dbcbe0f0900e0d154bef7cebfe8
Caramajau <caramia0505@gmail.com>
2024-04-25 15:27:48 +0200

    Added fishedTrash field to GameData

--- DAT257AgileProject/Assets/Scripts/DataPersistence/Data/GameData.cs
+++ DAT257AgileProject/Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -14,6 +14,10 @@ public class GameData
     private int money;
     public int Money { get { return money; } set { money = value; } }
 
+    [SerializeField]
+    private SerializableDictionary<TrashRarity, int> fishedTrash;
+    public SerializableDictionary<TrashRarity, int> FishedTrash { get { return fishedTrash; } set { fishedTrash = value; } }
+
     // This field is relevant for multiple files and debugging.
     [SerializeField]
     private long lastUpdated;
@@ -21,7 +25,7 @@ public class GameData
 
     public GameData()
     {
-        // should maybe have that the "correct" starting point for the player is (0, 0)?
+        // should maybe have that the "correct" starting point for the player be (0, 0)?
         playerPosition = new Vector2(-4, -2);
         money = 0;
     }
