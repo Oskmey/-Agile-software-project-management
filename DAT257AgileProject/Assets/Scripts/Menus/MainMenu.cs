991c397546f7d5aad4477a62e33f1a9eb717f219
Caramajau <caramia0505@gmail.com>
2024-04-25 16:17:16 +0200

    Created new methods in MainMenu

--- DAT257AgileProject/Assets/Scripts/Menus/MainMenu.cs
+++ DAT257AgileProject/Assets/Scripts/Menus/MainMenu.cs
@@ -2,29 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Button newGameButton;
+    [SerializeField]
+    private Button continueButton;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        // If there is no data shouldn't be able to continue.
+        if (!DataPersistenceManager.Instance.HasGameData())
+        {
+            continueButton.interactable = false;
+        }
     }
 
     public void PlayGame() 
     {
+        LoadScene();
+    }
+
+    public void OnNewGameClicked()
+    {
+        // Just to make sure the buttons aren't clicked more than once. 
+        DisableMenuButtons();
+        DataPersistenceManager.Instance.NewGame();
+        // TODO: Check if this is necessary.
+        DataPersistenceManager.Instance.SaveGame();
+        LoadScene();
+    }
+
+    private static void LoadScene()
+    {
+        // Loading the scene will load the game due to OnSceneLoaded in DataPersistenceManager.
         SceneManager.LoadSceneAsync("Oskar's World");
+    }
+
+    public void OnContinueClicked()
+    {
+        // Just to make sure the buttons aren't clicked more than once. 
+        DisableMenuButtons();
+        // Save game before loading scene. 
+        DataPersistenceManager.Instance.SaveGame();
+        LoadScene();
+    }
+
+    private void DisableMenuButtons()
+    {
+        newGameButton.interactable = false;
+        continueButton.interactable = false;
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
-
 }
