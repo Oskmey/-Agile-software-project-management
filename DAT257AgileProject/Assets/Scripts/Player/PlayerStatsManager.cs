e4bfcdcf92ff211db7f02a1569c08f45b3135847
Caramajau <caramia0505@gmail.com>
2024-04-30 09:38:44 +0200

    Remove unnecessary money field (only property now)

--- DAT257AgileProject/Assets/Scripts/Player/PlayerStatsManager.cs
+++ DAT257AgileProject/Assets/Scripts/Player/PlayerStatsManager.cs
@@ -4,28 +4,23 @@ using UnityEngine;
 
 public class PlayerStatsManager : MonoBehaviour, IDataPersistence
 {
-    private int money;
     public SerializableDictionary<TrashType, int> RecycledTrashDictionary { get; private set; }
 
-    public int Money
-    {
-        get { return money; }
-        set { money = value; }
-    }
+    public int Money { get; set; }
 
     public List<TrashType> FishedTrash { get; private set; }
 
     public void LoadData(GameData gameData)
     {
         RecycledTrashDictionary = gameData.RecycledTrashCount;
-        money = gameData.Money;
+        Money = gameData.Money;
         FishedTrash = gameData.FishedTrash;
     }
 
     public void SaveData(GameData gameData)
     {
         gameData.RecycledTrashCount = RecycledTrashDictionary;
-        gameData.Money = money;
+        gameData.Money = Money;
         gameData.FishedTrash = FishedTrash;
     }
 }
