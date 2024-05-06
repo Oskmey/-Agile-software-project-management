9fbbbeca4d444b1e7b668c5c7c9e769b156e8369
Caramajau <caramia0505@gmail.com>
2024-05-06 20:16:17 +0200

    Added some validation to FishingSpotRarities

--- DAT257AgileProject/Assets/Scripts/Fishing/FishingSpot.cs
+++ DAT257AgileProject/Assets/Scripts/Fishing/FishingSpot.cs
@@ -37,7 +37,15 @@ public class FishingSpot : MonoBehaviour
     //Need to add percentages I guess
     void Start()
     {
-        listOfRarities = new List<float>(fishingSpotRarities.ToList());
+        try
+        {
+            listOfRarities = new List<float>(fishingSpotRarities.ToList());
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"The following Exception occurred when loading fishingSpotRarities: {e}");
+        }
+        
         currentRarity = GetCurrentRarity(listOfRarities);
         promptText = GameObject.FindGameObjectWithTag("TutorialText").GetComponent<TextMeshProUGUI>();
         minigameManager = GameObject.FindGameObjectWithTag("Minigame Manager").GetComponent<MinigameManager>();
