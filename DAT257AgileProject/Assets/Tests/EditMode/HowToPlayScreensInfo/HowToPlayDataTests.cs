d38007fa52b590a4c67bceda3807ad7c4a0a9027
Caramajau <caramia0505@gmail.com>
2024-05-14 17:18:01 +0200

    Added some tests for the scriptable objects

---
+++ DAT257AgileProject/Assets/Tests/EditMode/HowToPlayScreensInfo/HowToPlayDataTests.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+[TestFixture]
+public class HowToPlayDataTests
+{
+    private static readonly HowToPlayData[] allHowToPlayData = Resources.LoadAll<HowToPlayData>("ScriptableObjects");
+
+    public static IEnumerable<TestCaseData> HowToPlayDataTestCases
+    {
+        get
+        {
+            foreach (var howToPlayDataEntry in allHowToPlayData)
+            {
+                yield return new TestCaseData(howToPlayDataEntry);
+            }
+        }
+    }
+
+    [Test]
+    public void AllHowToPlayData_IsNotNull()
+    {
+        Assert.IsNotNull(allHowToPlayData);
+    }
+
+    [Test]
+    public void AllHowToPlayData_IsNotEmpty()
+    {
+        Assert.IsNotEmpty(allHowToPlayData);
+    }
+
+    [Test, TestCaseSource(nameof(HowToPlayDataTestCases))]
+    public void EachHowToPlayData_HasScreenType(HowToPlayData howToPlayData)
+    {
+        Assert.IsNotNull(howToPlayData.ScreenType);
+    }
+
+    [Test, TestCaseSource(nameof(HowToPlayDataTestCases))]
+    public void EachHowToPlayData_HasContentText(HowToPlayData howToPlayData)
+    {
+        Assert.IsNotNull(howToPlayData.ContentText);
+    }
+
+    [Test, TestCaseSource(nameof(HowToPlayDataTestCases))]
+    public void EachHowToPlayData_HasNonEmptyContentText(HowToPlayData howToPlayData)
+    {
+        Assert.IsNotEmpty(howToPlayData.ContentText);
+    }
+
+    [Test, TestCaseSource(nameof(HowToPlayDataTestCases))]
+    public void EachHowToPlayData_HasContentTextStartingWithCapitalLetter(HowToPlayData howToPlayData)
+    {
+        Assert.AreEqual(char.ToUpper(howToPlayData.ContentText[0]), howToPlayData.ContentText[0]);
+    }
+
+    [Test, TestCaseSource(nameof(HowToPlayDataTestCases))]
+    public void EachHowToPlayData_DoesNotHaveContentTextEndingWithCarriageReturn(HowToPlayData howToPlayData)
+    {
+        Assert.AreNotEqual('\r', howToPlayData.ContentText[^1]);
+    }
+}
