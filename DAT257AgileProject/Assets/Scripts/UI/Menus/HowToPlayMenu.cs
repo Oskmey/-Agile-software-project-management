991bef6b0ff6d250a30363858178d7b0bd7b5e89
Caramajau <caramia0505@gmail.com>
2024-05-14 16:41:15 +0200

    Functionality to go back to main menu using esc

--- DAT257AgileProject/Assets/Scripts/UI/Menus/HowToPlayMenu.cs
+++ DAT257AgileProject/Assets/Scripts/UI/Menus/HowToPlayMenu.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -35,8 +36,12 @@ public class HowToPlayMenu : MonoBehaviour
     private static readonly HowToPlayScreenType defaultScreenType = HowToPlayScreenType.Controls;
     private IMenuWithHowToPlay otherMenuGameObject;
 
+    private PlayerInputActions playerInputActions;
+    private InputAction BackAction;
+
     private void Awake()
     {
+        playerInputActions = new PlayerInputActions();
         howToPlayScreenInformation = Resources.LoadAll<HowToPlayData>("ScriptableObjects");
     }
 
@@ -51,6 +56,23 @@ public class HowToPlayMenu : MonoBehaviour
         {
             Debug.LogError("otherMenuGameObject was null");
         }
+    }
+
+    private void OnEnable()
+    {
+        BackAction = playerInputActions.UI.Pause;
+        BackAction.Enable();
+        BackAction.performed += Back;
+    }
+
+    private void Back(InputAction.CallbackContext context)
+    {
+        OnBackButtonClicked();
+    }
+
+    private void OnDisable()
+    {
+        BackAction.Disable();
     }
 
     private void InitializeButtonListeners()
