a72c4a59b6662b970dd3f006b71162a65cd11435
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2024-05-15 12:24:04 +0200

    some changes to the money effect

--- DAT257AgileProject/Assets/Scripts/Accessory/Effects/MoneyEffectSO.cs
+++ DAT257AgileProject/Assets/Scripts/Accessory/Effects/MoneyEffectSO.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MoneyEffect", menuName = "Effects/Money Effect")]
 public class MoneyEffectSO : EffectSO
 {
+    [SerializeField]
+    float moneyMult;
     public override void ApplyEffect()
     {
         string folderPath = "Assets/Resources/ScriptableObjects/Trash";
@@ -21,7 +23,7 @@ public class MoneyEffectSO : EffectSO
                 TrashData trashData = (TrashData)asset;
 
                 // The effect
-                trashData.moneyValue *= 2;
+                trashData.moneyValue = (int)(trashData.moneyValue * (1 + moneyMult));
 
                 // dont know what this is lol
                 EditorUtility.SetDirty(asset);
