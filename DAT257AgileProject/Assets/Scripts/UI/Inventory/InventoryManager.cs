b323315d4b103b70e61aef8ce3f0cd43ad9f973d
Linus Karlsson <linuskungalv@gmail.com>
2024-05-16 16:39:55 +0200

    changed money multiplier effect text

--- DAT257AgileProject/Assets/Scripts/UI/Inventory/InventoryManager.cs
+++ DAT257AgileProject/Assets/Scripts/UI/Inventory/InventoryManager.cs
@@ -404,7 +404,7 @@ namespace Inventory
                             }
                             else if(accessoryEffect is MoneyEffectSO moneyEffectSO)
                             {
-                                effects += $"Money Multiplier: {1+moneyEffectSO.MoneyMult}x";
+                                effects += $"Money Multiplier: +{moneyEffectSO.MoneyMult}x";
                             }
                             else
                             {
